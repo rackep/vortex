555307b42c6f78d22abc52663e6fcf4ac35645df
Predrag Racanovic <racanovicp@yahoo.com>
2023-12-12 20:43:05 +0100

    2

--- main.tf
+++ main.tf
@@ -1,23 +1,4 @@
-# resource "aws_vpc" "vpc" {
-#   cidr_block           = var.vpc_cidr
-#   enable_dns_hostnames = true
-#   tags = {
-#     name = "main"
-#   }
-# }
-# resource "aws_subnet" "subnet" {
-#   vpc_id                  = aws_vpc.vpc.id
-#   cidr_block              = cidrsubnet(aws_vpc.vpc.cidr_block, 8, 1)
-#   map_public_ip_on_launch = true
-#   availability_zone       = "eu-central-1a"
-# }
 
-# resource "aws_subnet" "subnet2" {
-#   vpc_id                  = aws_vpc.vpc.id
-#   cidr_block              = cidrsubnet(aws_vpc.vpc.cidr_block, 8, 2)
-#   map_public_ip_on_launch = true
-#   availability_zone       = "eu-central-1b"
-# }
 
 data "aws_availability_zones" "available" {}
 
